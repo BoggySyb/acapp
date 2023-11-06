86a4c63503cf97db92c70b593cd7b2aa0839d415
boggy <syb2000417@163.com>
2023-11-06 09:50:50 +0800

    create structure

--- game/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(response):
-    line = '<h1 style="text-align: center">我的第一个网页</h1>'
-    return HttpResponse(line)
